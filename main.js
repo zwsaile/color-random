8ffb79e19532919327a4a9aa071c8f411103dacc
Zachary Saile <zwsaile@gmail.com>
2022-02-28 11:32:28 -0500

    Make final refactoring edits

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 var savedPalettes = []
-var colorBoxes = document.querySelectorAll(".square")
 var colorPalette = document.querySelector(".color-palette")
-var hexes = document.querySelectorAll(".hex")
-var unlock = document.querySelectorAll(".unlock-image")
 var newButton = document.querySelector(".new-palette")
 var savePaletteButton = document.querySelector(".save-palette")
 var miniSquareSection = document.querySelector(".mini-square")
-var squareOne = document.getElementById("color-one")
-var squareTwo = document.getElementById("color-two")
-var squareThree = document.getElementById("color-three")
-var squareFour = document.getElementById("color-four")
-var squareFive = document.getElementById("color-five")
-var hexOne = document.getElementById("hex-one")
-var hexTwo = document.getElementById("hex-two")
-var hexThree = document.getElementById("hex-three")
-var hexFour = document.getElementById("hex-four")
-var hexFive = document.getElementById("hex-five")
+var square = document.querySelectorAll(".square")
+var hexCodes = document.querySelectorAll(".hex")
 var miniTrash = document.getElementById("mini-trash")
+var unlock = document.querySelectorAll(".unlock-image")
 var hex = ["A", "B", "C", "D", "E", "F", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var currentPalette = [];
 
 newButton.addEventListener("click", changeAllColors)
 savePaletteButton.addEventListener("click", function() {
@@ -25,29 +16,25 @@ savePaletteButton.addEventListener("click", function() {
  changeAllColors()
 });
 window.addEventListener("load", changeAllColors)
+colorPalette.addEventListener("click", changeLockIcon);
 
-colorPalette.addEventListener("click", function() {
-  if (event.target.src !== "./assets/padlock.png") {
-    event.target.src = "./assets/padlock.png";
-  }
-});
-
-function changeColors(element, hexCode) {
-  var newColor = new Color
-    element.style.backgroundColor = newColor.hex
-    hexCode.innerText = newColor.hex
+function changeColors() {
+  var newPalette = [];
+  var newColorOne = new Color;
+  var newColorTwo = new Color;
+  var newColorThree = new Color;
+  var newColorFour = new Color;
+  var newColorFive = new Color;
+  newPalette.push(newColorOne, newColorTwo, newColorThree, newColorFour, newColorFive)
+  currentPalette = newPalette;
 };
 
 function changeAllColors() {
-  changeColors(squareOne, hexOne)
-  changeColors(squareTwo, hexTwo)
-  changeColors(squareThree, hexThree)
-  changeColors(squareFour, hexFour)
-  changeColors(squareFive, hexFive)
-};
-function addSavedPalette() {
-  savedPalettes.push(new Palette)
-  displayPalettes()
+  changeColors();
+  for (var i = 0; i < 5; i++) {
+    square[i].style.backgroundColor = currentPalette[i].hex;
+    hexCodes[i].innerText = currentPalette[i].hex;
+  };
 };
 
 function displayPalettes() {
@@ -65,6 +52,11 @@ function displayPalettes() {
   miniSquareSection.innerHTML = addHTML;
 };
 
+function addSavedPalette() {
+  savedPalettes.push(new Palette)
+  displayPalettes()
+};
+
 function deletePalette(id) {
   for (var i = 0; i < savedPalettes.length; i++) {
     if (id == savedPalettes[i].id) {
@@ -73,3 +65,9 @@ function deletePalette(id) {
   };
   displayPalettes()
 };
+
+function changeLockIcon() {
+  if (event.target.src !== "./assets/padlock.png") {
+    event.target.src = "./assets/padlock.png";
+  }
+}
